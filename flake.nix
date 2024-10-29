42c42dcb2ddb18697d4e0f46b135199806dd4f64
Stefanomarton <sstefanomarton@gmail.com>
2024-10-29 22:50:57 +0100

    chore: cleaner spacing

--- flake.nix
+++ flake.nix
@@ -2,7 +2,6 @@
   description = "A simple NixOS flake";
 
   inputs = {
-    # NixOS official package source, using the nixos-unstable branch here
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
@@ -14,16 +13,15 @@
       pkgs = nixpkgs.legacyPackages.${system};
       unstable = nixpkgs-unstable.legacyPackages.${system};
     in {
-      # Please replace my-nixos with your hostname
+      
       nixosConfigurations = {
         laptop = lib.nixosSystem {
           inherit system;
 
           modules = [
-            # Import the previous configuration.nix we used,
-            # so the old configuration file still takes effect
             ./configuration.nix
           ];
+          
           specialArgs = {
             inherit unstable;
           };

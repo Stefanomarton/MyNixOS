193475b31fb0352b526aed5c133183ef796f9741
Stefanomarton <sstefanomarton@gmail.com>
2025-02-06 17:48:32 +0100

    fix: hardware-configuration

--- hosts/desktop/hardware-configuration.nix
+++ hosts/desktop/hardware-configuration.nix
@@ -33,28 +33,29 @@
   boot.kernelParams = [
     "quiet"
     "amd_iommu=on"
+    "iommu=pt"
     "vfio-pci.ids=1002:67df,1002:aaf0"
+  ];
+  boot.blacklistedKernelModules = [
+    "ucsi_ccg"
   ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/8da45ec5-47e7-4ae9-8a22-74379a06cb6f";
-    fsType = "ext4";
-  };
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/ce5b1cc9-77ec-4c27-af91-9ee79d6b76f9";
+      fsType = "ext4";
+    };
 
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/8A73-8087";
-    fsType = "vfat";
-    options = [
-      "fmask=0077"
-      "dmask=0077"
-    ];
-  };
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/1C9B-FFE6";
+      fsType = "vfat";
+      options = [ "fmask=0077" "dmask=0077" ];
+    };
 
   fileSystems."/home" = {
-    device = "/dev/nvme0n1";
+    device = "/dev/disk/by-uuid/80c04392-ec38-4893-9936-36ded8e2a0c1";
     fsType = "btrfs";
-    options = [ "subvol=@home" ]; # Specify the subvolume name
+    options = [ "subvol=@home" ];
   };
 
   swapDevices = [ ];

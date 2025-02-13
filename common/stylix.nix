36ba6f708aebb8e98d92cf35f11f48e37a9c4275
Stefanomarton <sstefanomarton@gmail.com>
2025-02-14 00:30:54 +0100

    chore: add stylix

--- common/stylix.nix
+++ common/stylix.nix
@@ -1,3 +1,18 @@
 {
+  config,
+  pkgs,
+  unstable,
+  inputs,
+  stylix,
+  ...
+}:
+{
   stylix.enable = true;
+  stylix.autoEnable = false;
+  stylix.polarity = "dark";
+  stylix.image = ./wallpaper.png;
+  # stylix.targets = {
+  #   # firefox.enable = true;
+  #   # kitty.enable = true;
+  # };
 }

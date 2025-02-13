e98e802de7101f509ba530ee3edc224d3e75607c
Stefanomarton <sstefanomarton@gmail.com>
2025-02-14 00:06:02 +0100

    feat: introduce home-manager and stylix

--- flake.nix
+++ flake.nix
@@ -5,6 +5,12 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     zen-browser.url = "github:MarceColl/zen-browser-flake";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    stylix.url = "github:danth/stylix";
+
   };
 
   outputs =
@@ -12,6 +18,8 @@
       self,
       nixpkgs,
       nixpkgs-unstable,
+      home-manager,
+      stylix,
       ...
     }@inputs:
     let
@@ -28,17 +36,45 @@
 
           modules = [
             ./hosts/laptop/configuration.nix
+            ./common/stylix.nix
+            stylix.nixosModules.stylix
+            # make home-manager as a module of nixos
+            # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+
+              # TODO replace ryan with your own username
+              # home-manager.users.sm = import ./home.nix;
+
+              # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
+            }
           ];
 
           specialArgs = {
             inherit unstable;
           };
         };
+
         desktop = lib.nixosSystem {
           inherit system;
 
           modules = [
             ./hosts/desktop/configuration.nix
+            stylix.nixosModules.stylix
+            # make home-manager as a module of nixos
+            # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+
+              # TODO replace ryan with your own username
+              # home-manager.users.sm = import ./home.nix;
+
+              # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
+            }
           ];
 
           specialArgs = {
